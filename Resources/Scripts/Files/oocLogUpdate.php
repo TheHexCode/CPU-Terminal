592bb1a82cc060cd59ee85bc23140832786d1b82
TheHexCode <g.verroq@gmail.com>
2025-01-07 12:57:31 -0500

    OOC Action WIP 1.1

--- Resources/Scripts/Files/oocLogUpdate.php
+++ Resources/Scripts/Files/oocLogUpdate.php
@@ -3,7 +3,9 @@
 $suffixID = $_POST["suffixID"];
 $action = $_POST["actionJSON"];
 
+echo "console.log(". $action .");";
+
 # Timestamp | User | Action | Details
 $dataString = $action->timestamp . "," . $action->handle . "," . $action->action . "," . $action->details;
 
-file_put_contents("../../../Data/" . $suffixID . "/OOC_Action_Log.csv",$dataString,FILE_APPEND);
+file_put_contents("../../../Data/" . $suffixID . "/OOC_Action_Log.csv",PHP_EOL . $dataString,FILE_APPEND);
