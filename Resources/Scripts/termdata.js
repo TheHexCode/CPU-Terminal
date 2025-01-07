70a215178b1fa5e0b79bfbfb3d91d18fde0a9194
TheHexCode <g.verroq@gmail.com>
2025-01-07 00:58:57 -0500

    Action + Loading WIP

--- Resources/Scripts/termdata.js
+++ Resources/Scripts/termdata.js
@@ -1312,21 +1312,23 @@ function setupTerminalPage()
 {
 	let termState = terminal.getTermState().termState;
 
-	if(new Date(termState) != "Invalid Date") //rooting
-	{
+	console.log(termState);
 
-	}
-	else if(termState === "bricked")
+	if(new Date(termState) != "Invalid Date") // Rooting
 	{
 		
 	}
-	else if(termState == "rigged")
+	else if(Array.isArray(termState)) // Bricked
 	{
-
+		brickTerminal(termState);
 	}
-	else if(termState == "rooted")
+	/*else if(termState === "rigged")
 	{
 
+	}*/
+	else if(termState === "rooted")
+	{
+		rootTerminal();
 	}
 	else
 	{
@@ -1666,6 +1668,43 @@ function updateAccessLog(logIndex,action,reass)
 	$("#status").html(">> EXECUTING COMMAND...");
 }
 
+function brickTerminal(hexHandle)
+{
+	$("body").addClass("bricked");
+
+	let stopCode =  "0x000000" + hexHandle[0] + "<br/>" +
+					"(0x" + hexHandle[1] + hexHandle[2] + hexHandle[3] + hexHandle[4] + "," +
+					"0x" + hexHandle[5] + hexHandle[6] + hexHandle[7] + hexHandle[8] + ",<br/>" +
+					"&nbsp;0x" + hexHandle[9] + hexHandle[10] + hexHandle[11] + hexHandle[12] + "," +
+					"0x" + hexHandle[13] + hexHandle[14] + "0000)"
+
+	$("#main").html("<p>A problem has been detected and HexOS has been shut down to prevent damage to your device.</p>" +
+					"<p>UNMOUNTABLE_BOOT_VOLUME</p>" +
+					"<p>If this is the first time you've seen this error screen, restart your computer. If this screen appears again, follow these steps:" +
+					"<p>Check to make sure any new hardware or software is properly installed. If this is a new installation, ask your hardware or software manufacturer for any HexOS updates you might need.</p>" +
+					"<p>If problems continue, disable or remove any newly installed hardware or software. Disable BIOS memory options such as caching or shadowing. If you need to use Safe Mode to remove or disable components, restart your computer, press F8 to select Advanced Startup Options, and then select Safe Mode.</p>" +
+					"<p>Technical Information:</p>" +
+					"<p>*** STOP: " + stopCode + "</p>" +
+					"<p><br/>Beginning dump of physical memory<br/>" +
+					"Physical memory dump complete.<br/>" +
+					"Contact your system administrator or technical support group for further<br/>" +
+					"assistance.</p>" +
+					"<!--<footer>CPU DISCLAIMER</footer>-->"
+	);
+}
+
+function rootingTerminal(timeUp)
+{
+
+}
+
+function rootTerminal()
+{
+	$("#main").html("<p>HexOS BIOS</p>" +
+		"<!--<footer>CPU DISCLAIMER</footer>-->"
+	);
+}
+
 function executeAction(action)
 {
 	let cost;
@@ -1677,8 +1716,6 @@ function executeAction(action)
 		cost = 4;
 		timerSecs = payload.getTimerSecs();
 		callback = function() {
-			$("body").addClass("bricked");
-
 			let handle = payload.getHandle();
 			handle = (handle.masked) ? handle.mask : handle.handle;
 			handleLength = handle.length;
@@ -1695,25 +1732,7 @@ function executeAction(action)
 				hexHandle.push("00");
 			}
 
-			let stopCode =  "0x000000" + hexHandle[0] + "<br/>" +
-							"(0x" + hexHandle[1] + hexHandle[2] + hexHandle[3] + hexHandle[4] + "," +
-							"0x" + hexHandle[5] + hexHandle[6] + hexHandle[7] + hexHandle[8] + ",<br/>" +
-							"&nbsp;0x" + hexHandle[9] + hexHandle[10] + hexHandle[11] + hexHandle[12] + "," +
-							"0x" + hexHandle[13] + hexHandle[14] + "0000 )"
-
-			$("#main").html("<p>A problem has been detected and HexOS has been shut down to prevent damage to your device.</p>" +
-							"<p>UNMOUNTABLE_BOOT_VOLUME</p>" +
-							"<p>If this is the first time you've seen this error screen, restart your computer. If this screen appears again, follow these steps:" +
-							"<p>Check to make sure any new hardware or software is properly installed. If this is a new installation, ask your hardware or software manufacturer for any HexOS updates you might need.</p>" +
-							"<p>If problems continue, disable or remove any newly installed hardware or software. Disable BIOS memory options such as caching or shadowing. If you need to use Safe Mode to remove or disable components, restart your computer, press F8 to select Advanced Startup Options, and then select Safe Mode.</p>" +
-							"<p>Technical Information:</p>" +
-							"<p>*** STOP: " + stopCode + "</p>" +
-							"<p><br/>Beginning dump of physical memory<br/>" +
-							"Physical memory dump complete.<br/>" +
-							"Contact your system administrator or technical support group for further<br/>" +
-							"assistance.</p>" +
-							"<!--<footer>CPU DISCLAIMER</footer>-->"
-			);
+			brickTerminal(hexHandle);
 
 			payload.setCurrentTags(payload.getCurrentTags()-cost);
 			autoSave();
@@ -1726,7 +1745,7 @@ function executeAction(action)
 				{
 					suffixID: terminal.getTerminalID(),
 					path: "state",
-					newState: "bricked"
+					newState: hexHandle
 				}
 			});
 		};
@@ -1774,9 +1793,7 @@ function executeAction(action)
 			startTimer("EXECUTE",300,function () {
 				$("body").removeClass("rooting");
 
-				$("#main").html("<p>HexOS BIOS</p>" +
-					"<!--<footer>CPU DISCLAIMER</footer>-->"
-				);
+				rootTerminal();
 
 				$.ajax({
 					type: "POST",
