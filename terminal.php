d0e4b0ba1fcb9dbc9b67554b7a8631e372b751ed
TheHexCode <g.verroq@gmail.com>
2025-02-19 13:40:54 -0500

    sql GET

--- terminal.php
+++ terminal.php
@@ -2,11 +2,22 @@
 <html>
     <body>
         <?php
-            $pdo = new PDO('mysql:host=localhost;dbname=CPU_Terminal', '','');
-            $term_query = $pdo->query("SELECT * FROM CPU_Terminal.dbo.terminals"); #WHERE slug = slug AND job = job");
+            include('E:\Personal\Projects\CPU\dbConfig\dbConfig.php');
+
+            $idSlug = $_GET["id"];
+            $activeJob = "ABC1234";
+
+            $pdo = new PDO('sqlsrv:Server='.DBHOST.',1433;Database='.DBNAME, DBUSER,DBPWD);
+            $term_query = $pdo->query("SELECT * FROM CPU_Terminal.dbo.terminals WHERE CONVERT(varchar,slug) = '$idSlug' AND CONVERT(varchar,job) = '$activeJob'");
             $terminal = $term_query->fetch(PDO::FETCH_ASSOC);
 
-            echo $terminal['name'];
+            $entry_query = $pdo->query("SELECT * FROM CPU_Terminal.dbo.entries WHERE terminal_id={$terminal['id']}");
+            $entries = $entry_query->fetchAll(PDO::FETCH_ASSOC);
+
+            foreach($entries as $entry)
+            {
+                echo implode($entry);
+            }
         ?>
     </body>
 </html>
