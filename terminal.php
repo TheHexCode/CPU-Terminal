d9b3dc8f0bc5a1510535a3384079d4aab6dfcd33
TheHexCode <g.verroq@gmail.com>
2025-04-21 13:57:31 -0400

    active button WIP

--- terminal.php
+++ terminal.php
@@ -304,13 +304,16 @@
 								<div class="subContBody">
 									<ul id="actList">
 										<li id="brickItem" class="hidden">
-											BRICK
+											<span class="actionName">BRICK</span>
+											<button class="brickButton" data-enabled="true" data-cost="6" onclick="activeAction(this)">4 Tags</button>
 										</li>
 										<li id="riggItem" class="hidden">
-											RIGGED
+											<span class="actionName">RIGGED</span>
+											<button class="riggButton" data-enabled="true" data-cost="6" onclick="activeAction(this)">6 Tags</button>
 										</li>
 										<li id="rootItem" class="hidden">
-											ROOT DEVICE
+											<span class="actionName">ROOT DEVICE</span>
+											<button class="rootButton" data-enabled="true" data-cost="6" onclick="activeAction(this)">6 Tags</button>
 										</li>
 									</ul>
 								</div>
