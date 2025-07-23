14eba750f1a6d453fb52b9c21221f88ea55fd2e5
TheHexCode <g.verroq@gmail.com>
2025-07-23 14:30:21 -0400

    Start of Items Restructure

---
+++ resources/scripts/terminal/classes/items.js
@@ -0,0 +1,27 @@
+class Item
+{
+    static Budget_Cyberdeck_T0 = new Item({
+        name: "Budget Cyberdeck",
+        tier: 0
+    });
+    static Budget_Remote_Access_Drive_T0 = new Item("Budget Remote Access Drive",0);
+    static Canopic_Jar_Magsweep_T0       = new Item("Budget Cyberdeck",0);
+    static Budget_Cyberdeck_T0 = new Item("Budget Cyberdeck",0);
+    static Budget_Cyberdeck_T0 = new Item("Budget Cyberdeck",0);
+    static Budget_Cyberdeck_T0 = new Item("Budget Cyberdeck",0);
+    static Budget_Cyberdeck_T0 = new Item("Budget Cyberdeck",0);
+    static Budget_Cyberdeck_T0 = new Item("Budget Cyberdeck",0);
+    static Budget_Cyberdeck_T0 = new Item("Budget Cyberdeck",0);
+    static Budget_Cyberdeck_T0 = new Item("Budget Cyberdeck",0);
+    static Budget_Cyberdeck_T0 = new Item("Budget Cyberdeck",0);
+    static Budget_Cyberdeck_T0 = new Item("Budget Cyberdeck",0);
+    static Budget_Cyberdeck_T0 = new Item("Budget Cyberdeck",0);
+
+
+
+    constructor({name,tier}={})
+    {
+        console.log(name);
+        console.log(tier);
+    }
+}
